d66ad0d826d8f3c15f2c01a6606169e2ba49dc81
兀小雅 <zpyn1818@outlook.com>
2026-06-06 17:26:14 +0800

    Target AE2S-only 1.12.2 pattern API

--- versions/forge-1.12.2/src/main/java/com/ae2smartpatternsystem/Ae2RuntimeCompat.java
+++ versions/forge-1.12.2/src/main/java/com/ae2smartpatternsystem/Ae2RuntimeCompat.java
@@ -1,30 +1,14 @@
 package com.ae2smartpatternsystem;
 
 final class Ae2RuntimeCompat {
-    private static final String LEGACY_PATTERN_DETAILS =
-        "appeng.api.networking.crafting.ICraftingPatternDetails";
     private static final String AE2S_PATTERN_DETAILS =
         "appeng.api.crafting.IPatternDetails";
 
     private Ae2RuntimeCompat() {
     }
 
-    static boolean hasLegacyPatternApi() {
-        return hasClass(LEGACY_PATTERN_DETAILS);
-    }
-
     static boolean hasAe2sPatternApi() {
         return hasClass(AE2S_PATTERN_DETAILS);
-    }
-
-    static String describePatternApi() {
-        if (hasLegacyPatternApi()) {
-            return "legacy";
-        }
-        if (hasAe2sPatternApi()) {
-            return "ae2s";
-        }
-        return "missing";
     }
 
     private static boolean hasClass(String className) {
